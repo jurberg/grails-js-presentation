c48553ff721e56e71660dc4a4ae0326e80ff40b6
John Urberg <jurberg@gmail.com>
2013-11-28 23:13:18 -0600

    clean up code

--- example/web-app/js/app/todos.js
+++ example/web-app/js/app/todos.js
@@ -1,15 +1,13 @@
 // An example Backbone application contributed by
 // [Jérôme Gravel-Niquet](http://jgn.me/).
-(function(AppView, Global) {
+(function(AppView) {
 
   // Load the application once the DOM is ready, using `jQuery.ready`:
   $(function() {
 
-
-
     // Finally, we kick things off by creating the **App**.
-    Global.App = new AppView;
+    window.App = new AppView;
 
   });
 
-}(AppView, this));
+}(AppView));
